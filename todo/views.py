f56049319772950944c6be46cdb6f8ad6a33a868
alperkan1 <seanhack@gmail.com>
2022-01-03 16:51:59 +0000

    add .

--- todo/views.py
+++ todo/views.py
@@ -27,6 +27,15 @@ def add_item(request):
 
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+            'form': form
+    }
     form = ItemForm(instance=item)
     context = {
             'form': form
