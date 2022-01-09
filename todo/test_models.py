f29d38da203f6392599877e379ea068835c2f6ef
alperkan1 <seanhack@gmail.com>
2022-01-09 17:06:08 +0000

    add

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,4 +1,9 @@
 from django.test import TestCase
+from .models import Item
 
 
+class TestModels(TestCase):
 
+    def test_done_defalults_to_false(self):
+        item = Item.Objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
