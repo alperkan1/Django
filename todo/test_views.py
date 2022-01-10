f0275c4bf27c1e28e12694f680d7fbb5b242c75d
alperkan1 <seanhack@gmail.com>
2022-01-10 17:46:18 +0000

    commit

--- todo/test_views.py
+++ todo/test_views.py
@@ -30,10 +30,19 @@ class TestViews(TestCase):
         self.assertRedirects(response, '/')
         existin_items = Item.objects.filet(id=item.id)
         self.assertEqual(len(existing_items), 0)
-    
+
+
     def test_can_toggle_item(self):
         item = Item.objects.create(name='Test Todo Item')
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertFalse(updated_item.done)
+
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
